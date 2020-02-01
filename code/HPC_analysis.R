f3194c99269ebb975f547e55b16046184ba03ae4
Chanwoo Lee <35222069+Chanwoost@users.noreply.github.com>
2020-01-31 21:48:59 -0600

    Update HPC_analysis.R

--- code/HPC_analysis.R
+++ code/HPC_analysis.R
@@ -54,8 +54,9 @@ save(result,file = paste("CV_conti_",i,"_",j,".RData",sep = ""))
 
 
 ########## Analysis after getting the above output files ################
-CV = as.data.frame(matrix(nrow = 50, ncol = 3))
-names(CV) = c("MSE","MAE","Error_rate")
+   
+CV = as.data.frame(matrix(nrow = 50, ncol = 2))
+names(CV) = c("MAE","MCR")
 s = 0
 for(i in 1:10){
   for (j in 1:5) {
@@ -74,12 +75,12 @@ for(i in 1:10){
     test_index = cindex[[j]]
     load(paste("CV_conti_",i,"_",j,".RData",sep = ""))
     theta = result$theta
-    CV[s,1] = mean((round(theta)[test_index]-tensor[test_index])^2)
-    CV[s,2] = mean(abs(round(theta)[test_index]-tensor[test_index]))
-    CV[s,3] = error_rate = 1-length(which(round(theta)[test_index]- 
-                                            tensor[test_index]==0))/length(test_index)
+    CV[s,1] = mean(abs(theta[test_index]-tensor[test_index]))
+    CV[s,2] =  error_rate = mean(round(theta)[test_index]!=tensor[test_index])
   }
 }
+
+
 
 
 ##################### ordinal glm tucker decomposition CV#################
@@ -94,10 +95,36 @@ save(result,file = paste("CV_ordinal_",i,"_",j,".RData",sep = ""))
 
 
 ########## Analysis after getting the above output files ################
+### with median estimation
+OCVmedian = as.data.frame(matrix(nrow = 50, ncol = 2))
+names(OCVmedian) = c("MAE","MCR")
+s = 0
+for(i in 1:10){
+  for (j in 1:5) {
+    s = s+1
+    set.seed(i)
+    l1 = split(sample(which(tensor==1),length(which(tensor==1))),as.factor(1:5))
+    l2 = split(sample(which(tensor==2),length(which(tensor==2))),as.factor(1:5))
+    l3 = split(sample(which(tensor==3),length(which(tensor==3))),as.factor(1:5))
+    
+    cindex = list()
+    for (k in 1:5) {
+      cindex[[k]] = c(l1[[k]],l2[[k]],l3[[k]])
+    }
+    
+    test_index = cindex[[j]]
+    test_index = cindex[[j]]
+    load(paste("CV_ordinal_",i,"_",j,".RData",sep = ""))
+    theta = result$theta
+    out = estimation(theta,result$omega,type="median")@data # we can also use other estimator via type="mode","mean", or "median"
+    OCVmedian[s,1] = mean(abs(out[test_index]-tensor[test_index]))
+    OCVmedian[s,2] = error_rate = mean(out[test_index]!=tensor[test_index])
+  }
+}
 
-
-OCV = as.data.frame(matrix(nrow = 50, ncol = 3))
-names(OCV) = c("MSE","MAE","Error_rate")
+### with mode estimaiton
+OCVmode = as.data.frame(matrix(nrow = 50, ncol = 2))
+names(OCVmode) = c("MAE","MCR")
 s = 0
 for(i in 1:10){
   for (j in 1:5) {
@@ -117,10 +144,8 @@ for(i in 1:10){
     load(paste("CV_ordinal_",i,"_",j,".RData",sep = ""))
     theta = result$theta
     out = estimation(theta,result$omega,type="mode")@data # we can also use other estimator via type="mode","mean", or "median"
-    OCV[s,1] = mean((out[test_index]-tensor[test_index])^2) # if we use type ="mode",  can we not use "round" function?
-    OCV[s,2] = mean(abs(out[test_index]-tensor[test_index]))
-    OCV[s,3] = error_rate = mean(out[test_index]!=tensor[test_index])
+    OCVmode[s,1] = mean(abs(out[test_index]-tensor[test_index]))
+    OCVmode[s,2] = error_rate = mean(out[test_index]!=tensor[test_index])
   }
 }
-
 
